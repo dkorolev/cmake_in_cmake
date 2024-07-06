8906ca7837138dcad5c974f4ec38127b5a179c97
Dima Korolev <dmitry.korolev+github-2022@gmail.com>
2024-07-06 17:55:53 +0800

    Added `std::flush`.

--- src/example.cc
+++ src/example.cc
@@ -4,7 +4,7 @@
 int main() {
   for (int i = 1; i <= 3; ++i) {
     for (int j = 1; j <= 3; ++j) {
-      std::cout << ' ' << c5t_lib_mul(i, j);
+      std::cout << ' ' << c5t_lib_mul(i, j) << std::flush;
     }
     std::cout << std::endl;
   }
