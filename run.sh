4154244b73a1a309a8e3d1224c654a7b93709887
Dima Korolev <dmitry.korolev+github-2022@gmail.com>
2024-07-06 15:33:20 +0800

    Added more test cases for re-getting the `cmake/CMakeLists.txt` file into the `run.sh` script.

--- run.sh
+++ run.sh
@@ -11,4 +11,21 @@ for RUN in "cmake -B build" "cmake --build build" build/cmake/hw build/cmake/tes
   echo
 done
 
+echo '# Making sure the `cmake/CMakeLists.txt` file is not modified unnecessarily.'
+echo
+for RUN in "stat cmake/CMakeLists.txt" "cmake -B build" "stat cmake/CMakeLists.txt" ; do
+  echo '$ '${RUN}
+  ${RUN}
+  echo
+done
+
+echo '# Making sure the altered `cmake/CMakeLists.txt` is overwritten.'
+echo
+echo "# Dummy." >> cmake/CMakeLists.txt
+for RUN in "stat cmake/CMakeLists.txt" "cmake -B build" "stat cmake/CMakeLists.txt" ; do
+  echo '$ '${RUN}
+  ${RUN}
+  echo
+done
+
 echo '# All done.'
